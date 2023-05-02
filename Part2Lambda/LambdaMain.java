f399654140265136c034a18f9d3d8776b4d77206
Skilly146 <third.landon.davidson4@gmail.com>
2023-05-02 11:20:14 -0700

    Add: Printout of functions

--- Part2Lambda/LambdaMain.java
+++ Part2Lambda/LambdaMain.java
@@ -9,6 +9,8 @@ the ArrayProcessor functional interface
 
 package Part2Lambda;
 
+import java.util.Arrays;
+
 public class LambdaMain {
     public static void main(String[] args) {
         double[] firstList = { 6, 2.6, 3, 5, 7, 12, 7, 2.5, 10.3, 1.7 };
@@ -46,6 +48,11 @@ public class LambdaMain {
             return total / array.length;
         };
 
-        System.out.println(averageOfElements.apply(firstList));
+        System.out.println("List: " + Arrays.toString(firstList));
+        System.out.println("Amount of Elements: " + numberOfElements.apply(firstList));
+        System.out.println("Maximum Value: " + maxVal.apply(firstList));
+        System.out.println("Minimum Value: " + minVal.apply(firstList));
+        System.out.println("Sum of Elements: " + sumOfElements.apply(firstList));
+        System.out.println("Average of Elements: " + averageOfElements.apply(firstList));
     }
 }
