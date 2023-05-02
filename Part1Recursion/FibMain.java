b8cc55fafd6e4d830e9edba0060346c8199e3c49
Skilly146 <third.landon.davidson4@gmail.com>
2023-05-02 11:23:36 -0700

    Fix: Output

--- Part1Recursion/FibMain.java
+++ Part1Recursion/FibMain.java
@@ -12,13 +12,13 @@ package Part1Recursion;
 public class FibMain {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        System.out.println("Recursive: " + RecursiveFib.fib(45));
+        System.out.println("Recursive Fib of 45: " + RecursiveFib.fib(45));
         long finish = System.currentTimeMillis();
         long timeElapsed = finish - start;
         System.out.println(timeElapsed + " ms");
 
         start = System.currentTimeMillis();
-        System.out.println("Iterative: " + IterativeFib.fib(45));
+        System.out.println("Iterative Fib of 45: " + IterativeFib.fib(45));
         finish = System.currentTimeMillis();
         timeElapsed = finish - start;
         System.out.println(timeElapsed + " ms");
